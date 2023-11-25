aa0df1e42399c52c7e547913c1c54632392ad07a
Oskar Rehbein <151864316+OskarRehbein@users.noreply.github.com>
2023-11-25 17:49:04 -0300

    Add files via upload

---
+++ Solemne 2.py
@@ -0,0 +1,75 @@
+
+import random
+
+numero_colaboradores = int(input("Ingrese el numero de colaboradores: "))
+dias_del_mes = 30
+turnos_colaboradores = [[False]*dias_del_mes for _ in range(numero_colaboradores)]
+pago_por_hora = int(input("Ingrese el pago por hora en CLP: "))
+horas_por_turno = int(input("Ingrese la duracion de un turno laborar: "))
+
+lista_de_turno = []
+backup_lista_de_turno = []
+matriz_paga = []
+
+def mostrar_matriz():
+    for i in range(len(turnos_colaboradores)):
+        for j in range(len(turnos_colaboradores[0])):
+            print(turnos_colaboradores[i][j], end=" ")
+        print()
+
+def informacion_fin_de_mes():
+    for i in range(numero_colaboradores):
+        print(f"El colaborador {i+1} trabajo los dias: ", end=" ")
+        pago = horas_por_turno * pago_por_hora * turnos_trabajados[i]
+        for j in range(dias_del_mes):
+            if turnos_colaboradores[i][j] == True:
+                print(f"{j+1} ", end="")
+        print(f"del mes ({turnos_trabajados[i]} turnos / {turnos_trabajados[i] * horas_por_turno} horas trabajadas) para una paga de {pago} CLP")
+        matriz_paga.append(pago)
+
+for dia in range(dias_del_mes):
+    if dia == 6:
+        continue
+    if dia == 13:
+        continue
+    if dia == 20:
+        continue
+    if dia == 27:
+        continue
+
+    while True:
+        x = random.randint(0, numero_colaboradores-1)
+        #print(x)
+        if not x in lista_de_turno:
+            lista_de_turno.append(x) # type: ignore
+            break
+    print(lista_de_turno)
+        
+    turnos_colaboradores[lista_de_turno[len(lista_de_turno)-1]][dia] = True
+    mostrar_matriz()
+
+    if len(lista_de_turno) == numero_colaboradores:
+        lista_de_turno.clear()
+
+        #Intento limpieza de turnos
+        """print(lista_de_turno)
+        backup_lista_de_turno.append(lista_de_turno[len(lista_de_turno)-1])
+        print(backup_lista_de_turno)
+        lista_de_turno.clear()
+        lista_de_turno.append(backup_lista_de_turno[len(backup_lista_de_turno)-1])"""
+
+turnos_trabajados = []
+suma_turnos = 0
+for colaboradores in range(0,numero_colaboradores):
+    for dias in range(0,dias_del_mes):
+        suma_turnos += turnos_colaboradores[colaboradores][dias]
+    turnos_trabajados.append(suma_turnos)
+    suma_turnos = 0
+    print(turnos_trabajados)
+
+informacion_fin_de_mes()
+pago_final = 0
+for i in range(len(matriz_paga)):
+    pago_final += matriz_paga[i]
+
+print(f"El costo final del mes es de {pago_final} CLP")
